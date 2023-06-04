554537bf0922c0f4cb406a93841c9eef3deb1613
Cristiano Marques <cristiano.marques@gmail.com>
2023-06-04 09:28:24 -0300

    Aula 2
    atualizações aula 2

--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -1,14 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("campeonatobrasileiro.controller.Main", {
             onInit: function () {
+                //vamos criar um modelo
+                //antes, as variaveis que vão no modelo
+                var dadosGerais = {
+                    rodada : '89a',
+                    campeonato : "Brasileirão 2023 do Canal FioriNET"
+                };
+
+                // agora sim vamos criar o modelo
+                var dadosModel = new JSONModel();
+                dadosModel.setData(dadosGerais);
+                var view = this.getView();
+                view.setModel(dadosModel, "ModeloDadosGerais");
+
+                
+
 
             }
         });
